335d048c4eeb10f299cff6fbb020d0472e61de68
allfungit <74088537+allfungit@users.noreply.github.com>
2023-05-03 09:54:18 +0700

    Update build_push_image.sh

--- build_push_image.sh
+++ build_push_image.sh
@@ -10,7 +10,8 @@ docker images
 docker tag item-app:v1 docker.pkg.github.com/allfungit/a433-microservices/tree/proyek-pertama/item-app:v1
 
 # Login ke GitHub Packages via Terminal
-docker login docker.pkg.github.com -u allfungit -p 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
+docker login docker.pkg.github.com -u allfungit -p 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
 
 # Mengunggah image ke GitHub Packages
 docker push docker.pkg.github.com/allfungit/a433-microservices/tree/proyek-pertama/item-app:v1
+
