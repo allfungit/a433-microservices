ccae73eda110c06c54bc19358b1c455aa74839c2
allfungit <maulanaalfan21@gmail.com>
2023-05-19 10:03:09 +0700

    Menambahkan folder lokal ke cabang proyek-pertama

--- build_push_image.sh
+++ build_push_image.sh
@@ -6,12 +6,12 @@ docker build -t item-app:v1 .
 # Melihat daftar image di lokal
 docker images
 
-# Mengubah nama image agar sesuai dengan format GitHub Packages
-docker tag item-app:v1 docker.pkg.github.com/allfungit/a433-microservices/tree/proyek-pertama/item-app:v1
+# Mengubah nama image agar sesuai dengan format Docker Hub
+docker tag item-app:v1 alfandocker/item-app:v1
 
-# Login ke GitHub Packages via Terminal
-docker login docker.pkg.github.com -u allfungit -p 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
+# Login ke Docker Hub via Terminal
+echo $PASSWORD_DOCKER_HUB | docker login -u alfandocker --password-stdin
 
-# Mengunggah image ke GitHub Packages
-docker push docker.pkg.github.com/allfungit/a433-microservices/tree/proyek-pertama/item-app:v1
+# Mengunggah image ke Docker Hub
+docker push alfandocker/item-app:v1
 
